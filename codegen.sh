20f36c151aa94f7a72bd17ad91afba467c0454d7
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-07-26 15:13:27 +0800

    Chore: Add script to fix import issue in graphql generated file

---
+++ codegen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+graphql-codegen
+
+# DECLARE VARIABLES
+GENERATED_DIR="src/graphql"
+GENERATED_FILE=$(find "$GENERATED_DIR" -type f -name generated.ts)
+
+MATCH="import"
+REPLACE="import type"
+
+read -r -d '' COMMENT <<'EOF'
+// Make sure to use import type { xxx } from '...' instead of import { type xxx } from '...', 
+// because only the former will be dropped completely and thereby not incur any issues.
+EOF
+
+
+if [ -z "$GENERATED_FILE" ]; then
+  echo "NO GENERATED FILE FOUND!!!"
+  exit 1
+fi
+
+echo "GENERATED FILE FOUND. EDITING THE FILE..."
+
+# Fix import issue
+sed -i "1 s/$MATCH/$REPLACE/" src/graphql/generated.ts
+
+# Add necessary comment
+echo "$COMMENT" | cat - src/graphql/generated.ts > generated.ts && mv generated.ts src/graphql/generated.ts
+
+
