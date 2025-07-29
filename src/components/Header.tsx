be55423ef9a0125e54ee2d75fec4db628de047a1
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-07-29 16:42:29 +0800

    Chore: Adjust font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ import type { ReactElement } from "react"
 
 function Header({ children }: { children?: ReactElement | ReactElement[] }) {
   return (
-    <div className='flex flex-row items-center bg-[#bebbbb] min-w-full pl-[2rem] p-[1rem]'>
+    <div className='flex flex-row items-center bg-[#dddddd] min-w-full pl-[2rem] p-[1.2rem]'>
       <p className='size-[56px] '>
         <img className="size-full" src={foldedHandsIcon} alt="#" />
       </p>
-      <div className='flex flex-row pl-[1rem]'>
+      <div className='flex flex-row pl-[1rem] tracking-[0.12em] text-[20px] font-logo'>
         <strong>Sprint</strong> Lang
       </div>
       {children}
