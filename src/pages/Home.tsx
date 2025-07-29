7bfb76a55ec5be1f2086fa379a07e45c91cc2981
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-07-29 14:06:24 +0800

    Build: Change the contents of Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,23 @@
-import { useMutation, useQuery } from 'urql'
-import { GetUsersDocument, AddUsersDocument, type GetUsersQuery } from "../graphql/generated"
-import UserDisplay from "../components/UserDisplay"
-import { useEffect, useState } from 'react'
 import type { Route } from "./+types/Home"
 
+// import UserDisplay from "../components/UserDisplay"
+import Header from '../components/Header'
+import { Link } from "react-router"
+
 export default function Home(args: Route.ComponentProps) {
-  console.log("args.actionData: ", args.actionData)
-  const [getUsersResult] = useQuery({ query: GetUsersDocument })
-  const [addUserResult] = useMutation(AddUsersDocument)
-  // const [addUserResult, addUser] = useMutation(AddUsersDocument)
-  const [users, setUser] = useState<GetUsersQuery["users"]>([])
-
-  // Initialize users once
-  useEffect(() => {
-    const users = getUsersResult.data?.users;
-    if (users) setUser([...users])
-
-  }, [getUsersResult.data?.users])
-
-  // Append new user when mutation completes
-  useEffect(() => {
-    const newUser = addUserResult.data?.addUsers
-    if (newUser) setUser((prev) => [...(prev ?? []), newUser])
-
-  }, [addUserResult.data?.addUsers])
-
-  // const refresh = () => {
-  //   // Re-execute the query with a network-only policy, skipping the cache
-  //   // getUsers({ requestPolicy: 'network-only' });
-  // };
-
-  // const handleSubmit = () => {
-  //   addUser({ name: "Naruto" }).then(result => {
-  //     if (result.error)
-  //       return console.error(`${result.error}`)
-  //   })
-  // }
-
-  function PageContent() {
-    if ((getUsersResult.fetching || addUserResult.fetching) && (!getUsersResult.error || !addUserResult.error)) return <p>Loading...</p>
-    if (getUsersResult.error || addUserResult.error) return <p>{`error.message sdfsdffsdf`}</p>
-
-    if (users)
-      return <>
-        {users.map((user, i) => <UserDisplay user={user} key={i} />)}
-        {/* <button onClick={handleSubmit}>Add Sakura</button> */}
-      </>
-  }
+  console.log("Home args: ", args)
 
   return (
-    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-      <PageContent />
+    <div className="bg-[#e7e7e7] flex-col h-screen w-full flex items-center gap-y-0.5 overflow-hidden">
+      <Header />
+
+      <main className="w-full h-full flex flex-col justify-center-safe items-center-safe">
+        <p className="text-[30px] text-[#000000]">Welcome</p>
+
+        <div className="flex flex-row">
+          Let's <Link to={"/play"} className="pl-[5px] pr-[5px] hover:text-gray-700"> play,</Link> <p>Jade</p>.
+        </div>
+      </main>
     </div>
   )
 }
