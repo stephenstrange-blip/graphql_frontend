985b4adfa43e3cec57de48938761114eba815cd8
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-08-31 19:09:35 +0800

    Refactor: Manage settings in unified state   - Add reset button   - Combine numRounds, from, and to language IDs into one state to allow reset   - Adjust Dropdown and HorizontalNumberInput props accordingly

--- src/pages/sections/PreparationSection.tsx
+++ src/pages/sections/PreparationSection.tsx
@@ -1,13 +1,24 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect, type FormEventHandler } from "react";
 import { Form } from "react-router";
 import type { langTranslate } from "../../types/types";
 import { SubmitContext } from "../../context/context";
 import { HorizontalNumberInput } from "../../components/Input";
 import { Dropdown } from "../../components/Dropdown";
 
-export function PreparationSection({ to, from, numRounds, onSubmit }: { to: langTranslate, from: langTranslate, numRounds: number, onSubmit: React.FormEventHandler<HTMLFormElement> }) {
-  const [value, setValue] = useState<number>(numRounds);
+export function SettingSection({ to, from, numRounds, onSubmit }: { to: langTranslate, from: langTranslate, numRounds: number, onSubmit: React.FormEventHandler<HTMLFormElement> }) {
   const { isFetching } = useContext(SubmitContext)
+  const [settings, setSettings] = useState({ to: to.id, from: from.id, numRounds })
+
+  // After render, data may not be available immediately 
+  // since it depends on data from the server. 
+  // Re-run whenever either one changes.
+  useEffect(() => {
+    setSettings({
+      to: to.id,
+      from: from.id,
+      numRounds: numRounds
+    })
+  }, [numRounds, from.id, to.id])
 
   const langugageNames = {
     eng: "english",
@@ -23,42 +34,50 @@ export function PreparationSection({ to, from, numRounds, onSubmit }: { to: lang
     { code: "kor", id: 3 }
   ]
 
-  useEffect(() => {
-    setValue(numRounds)
-  }, [numRounds])
-
   const languageWithNames = languages.map(item => ({
     code: item.code,
     id: item.id,
     name: langugageNames[item.code as keyof typeof langugageNames]
   }))
 
+  // In case of failed gamesetting update, 
+  // or client wants to reset the game settings
+  const reset = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setSettings({ to: to.id, from: from.id, numRounds: numRounds })
+    // prevent form submission
+    return
+  }
+
   return (
     <Form onSubmit={onSubmit} className="p-5 flex-col flex items-center-safe gap-6">
-      <HorizontalNumberInput id="numRounds" value={value} onChange={setValue}>
+      <HorizontalNumberInput id="numRounds" value={settings.numRounds} onChange={setSettings}>
         <label htmlFor="numRounds" className="text-center">Number of Rounds</label>
       </HorizontalNumberInput>
       <span className="flex flex-row gap-50">
         <div className="flex" >
           <label htmlFor="from">From: </label>
           <Dropdown
-            initial={from}
+            initialId={settings.from}
             languages={languageWithNames}
             option="from"
+            setValue={setSettings}
           />
         </div>
         <div className="flex">
           <label htmlFor="to">To: </label>
           <Dropdown
-            initial={to}
+            initialId={settings.to}
             languages={languageWithNames}
             option="to"
+            setValue={setSettings}
           />
         </div>
       </span>
 
       <div className="flex flex-row gap-2 justify-center-safe items-center-safe">
         <button className="hover:bg-gray-300 p-2.5 rounded-[5px] min-w-15" type="submit" disabled={!!isFetching}>{isFetching ? "Applying..." : "Apply"}</button>
+        <button className="hover:bg-gray-300 p-2.5 rounded-[5px] min-w-15" onClick={reset}>Reset</button>
       </div>
     </Form>
   )
